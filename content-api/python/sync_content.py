fec8003a8163d1ab7e2c44ca07cdca62e5847091
evgeny.perov <evgeny.perov@microimpuls.com>
2024-11-25 14:56:49 +0500

    fix: Добавлены тайпхинты, оптимизирован процесс поиска сериала по id (#21309)

--- content-api/python/sync_content.py
+++ content-api/python/sync_content.py
@@ -66,16 +66,16 @@ class SyncContent(object):
         self.seasons = {}
         self.episodes = {}
 
-    def get_pages(self, limit, count):
+    def get_pages(self, limit: int, count: int) -> int:
         pages = count//limit
         if count % limit != 0:
             pages += 1
         return pages
 
-    def is_updated(self, video):
+    def is_updated(self, video: dict) -> bool:
         return video['has_seasons'] and video['has_episodes']
 
-    def parse_vf_name(self, vf_name):
+    def parse_vf_name(self, vf_name: str):
         match = re.match(VIDEO_FILE_REGEX, vf_name)
         if match and len(match.group(0)) == len(vf_name):
             season_number = int(match.group(1))
@@ -83,12 +83,12 @@ class SyncContent(object):
             return season_number, episode_number
         raise ParseError
 
-    def create_season(self, season_number, vf_vid):
+    def create_season(self, season_number: int, vf_vid: int):
         if season_number not in self.seasons.keys():
             s = Season(self.api, season_number, vf_vid)
             self.seasons[season_number] = s.create()
 
-    def create_episode(self, season_number, episode_number, vf):
+    def create_episode(self, season_number: int, episode_number: int, vf: VideoFile):
         if vf.name not in self.episodes.keys():
             season_id = self.seasons[season_number]
             e = Episode(
@@ -101,7 +101,7 @@ class SyncContent(object):
             self.episodes[vf.name] = e.create()
         vf.episode_id = self.episodes[vf.name]
 
-    def update_videofile(self, vf, vid):
+    def update_videofile(self, vf: dict, vid: int) -> bool:
         vf_obj = VideoFile(
             self.api,
             id=vf['id'],
@@ -121,27 +121,25 @@ class SyncContent(object):
         vf_obj.modify()
         return True
 
-    def update_content(self, videos):
+    def update_content(self, videos: list):
         for video in videos:
             self.seasons = {}
             self.episodes = {}
             if self.is_updated(video):
                 continue
-            video_files = video['files']
-            is_success = True
-            for vf in video_files:
-                is_success = (
-                    self.update_videofile(vf, video['id']) and
-                    is_success
-                )
-            if video_files and is_success:
-                print('Video id={} updated successfully'.format(video['id']))
-            elif video_files:
-                print('Video id={} update failed'.format(video['id']))
 
-    def update_video_content(self, videos, video_id):
+            video_files = video['files']
+            is_success = all(self.update_videofile(vf, video['id']) for vf in video_files)
+
+            if video_files:
+                if is_success:
+                    print('Video id={} updated successfully'.format(video['id']))
+                else:
+                    print('Video id={} update failed'.format(video['id']))
+
+    def update_video_content(self, videos: list, video_id: int) -> bool:
         """
-        Возвращает True, если указанный VIDEO_ID обработан
+        Возвращает True, если указанный VIDEO_ID найден
         """
         for video in videos:
             if video['id'] != video_id:
@@ -150,18 +148,16 @@ class SyncContent(object):
             self.episodes = {}
             if self.is_updated(video):
                 continue
+
             video_files = video['files']
-            is_success = True
-            for vf in video_files:
-                is_success = (
-                    self.update_videofile(vf, video['id']) and
-                    is_success
-                )
-            if video_files and is_success:
-                print('Video id={} updated successfully'.format(video['id']))
+            is_success = all(self.update_videofile(vf, video['id']) for vf in video_files)
+
+            if video_files:
+                if is_success:
+                    print('Video id={} updated successfully'.format(video['id']))
+                else:
+                    print('Video id={} update failed'.format(video['id']))
                 return True
-            elif video_files:
-                print('Video id={} update failed'.format(video['id']))
         return False
 
     def load_data(self):
@@ -179,16 +175,16 @@ class SyncContent(object):
         Обработка сериала по VIDEO_ID
         """
         limit = 100
-        response = self.api.video_list(page=1, limit=limit)
-        updated = self.update_video_content(response['videos'], VIDEO_ID)
-        if not updated and response['count'] > limit:
-            pages = self.get_pages(limit, response['count'])
-            for i in range(2, pages+1):
-                response = self.api.video_list(page=i, limit=limit)
-                updated = self.update_video_content(
-                    response['videos'],
-                    VIDEO_ID,
-                )
+        page = 1
+        while True:
+            response = self.api.video_list(page=page, limit=limit)
+            updated = self.update_video_content(response['videos'], VIDEO_ID)
+            if not response['videos']:
+                print('Error: video not found')
+                break
+            if updated:
+                break
+            page += 1
 
 
 if __name__ == '__main__':
